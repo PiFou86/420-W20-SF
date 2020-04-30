bae9d1b69c8a95a163cb1967c8473a82739d4be1
Pierre-François Léon <pierrefrancois.leon@gmail.com>
2020-04-30 17:29:06 -0400

    Correction classe joueur

--- Module11_Demeter_TellDontAsk/POOI_Module11_JeuTir/POOI_Module11_JeuTir/Joueur/Joueur.cs
+++ Module11_Demeter_TellDontAsk/POOI_Module11_JeuTir/POOI_Module11_JeuTir/Joueur/Joueur.cs
@@ -19,8 +19,6 @@ namespace POOI_Module11_JeuTir.Joueur
         }
 
         private Arme m_armeSelectionnee;
-        private Vecteur3D m_directionRegard;
-        private Point3D m_position;
 
         public MoteurSon MoteurSon { get; private set; }
 
@@ -29,8 +27,8 @@ namespace POOI_Module11_JeuTir.Joueur
             this.m_armes = new List<Arme>();
             this.m_armes.Add(new Stylo());
             this.m_armeSelectionnee = this.Armes[0];
-            this.m_position = new Point3D() { X = 0, Y = 0, Z = 0 };
-            this.m_directionRegard = new Vecteur3D() { X = 1, Y = 0, Z = 0 };
+            this.Position = new Point3D() { X = 0, Y = 0, Z = 0 };
+            this.Direction = new Vecteur3D() { X = 1, Y = 0, Z = 0 };
         }
 
         public void RamasserArme(Arme p_arme)
@@ -42,7 +40,7 @@ namespace POOI_Module11_JeuTir.Joueur
         {
             if (this.m_armeSelectionnee.Temperature < 250)
             {
-                CollisionTir col = this.m_armeSelectionnee.Tirer(this.m_position, this.m_directionRegard);
+                CollisionTir col = this.m_armeSelectionnee.Tirer(this.Position, this.Direction);
                 if (col != null)
                 {
                     col.ObjetTouche.AppliquerDomage(col.Degat);
